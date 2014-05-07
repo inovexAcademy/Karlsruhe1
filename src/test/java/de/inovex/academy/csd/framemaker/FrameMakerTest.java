cd0da8e109f2e1f8db89a0e0de8faa98c36db1f2
philipp-bender <philipp_bender@gmx.net>
2014-05-07 14:28:38 +0200

    Refactor frame maker test

--- src/test/java/de/inovex/academy/csd/framemaker/FrameMakerTest.java
+++ src/test/java/de/inovex/academy/csd/framemaker/FrameMakerTest.java
@@ -1,44 +1,39 @@
 package de.inovex.academy.csd.framemaker;
 
-import static org.junit.Assert.*;
 import static java.util.Arrays.asList;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
-import java.util.Arrays;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class FrameMakerTest {
 
+	private FrameMaker maker;
+	
 	@Before
 	public void setUp() throws Exception {
+		maker = new FrameMaker();
 	}
 
 	@Test
-	public void testFrameMakerWithSingleLetter() {
-				FrameMaker maker = new FrameMaker();
-		
+	public void testFrameMakerWithSingleLineAndSingleLetter() {
 		assertThat(maker.makeFrame(asList("x")), equalTo(asList("***","*x*","***")));
 		assertThat(maker.makeFrame(asList("y")), equalTo(asList("***","*y*","***")));
 	}
 
 	@Test
-	public void testFrameMakerWithSingleWord() {
-		FrameMaker maker = new FrameMaker();
+	public void testFrameMakerWithSingleLineAndSingleWord() {
 		assertThat(maker.makeFrame(asList("hello")), equalTo(asList("*******","*hello*","*******")));
 	}
 	
 	@Test
 	public void testFrameMakerWithMultiLines() {
-		FrameMaker maker = new FrameMaker();
 		assertThat(maker.makeFrame(asList("hello", "world")), equalTo(asList("*******","*hello*","*world*","*******")));
 	}
 	
 	@Test
 	public void testFrameMakerWithMultiLinesAndDifferentLength() {
-		FrameMaker maker = new FrameMaker();
 		assertThat(maker.makeFrame(asList("hello", "world!!!")), equalTo(asList("**********","*hello   *","*world!!!*","**********")));
 	}
 }
